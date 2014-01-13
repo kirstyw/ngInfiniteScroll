1eec41892574640ae898c98158d1c6faf5038c0e
kirstyw <kirsty20@gmail.com>
2014-01-13 17:24:26 +0000

    Update ng-infinite-scroll.js

--- build/ng-infinite-scroll.js
+++ build/ng-infinite-scroll.js
@@ -1,4 +1,3 @@
-/* ng-infinite-scroll - v1.0.0 - 2013-02-23 */
 var mod;
 
 mod = angular.module('infinite-scroll', []);
@@ -6,54 +5,57 @@ mod = angular.module('infinite-scroll', []);
 mod.directive('infiniteScroll', [
   '$rootScope', '$window', '$timeout', function($rootScope, $window, $timeout) {
     return {
-      link: function(scope, elem, attrs) {
-        var checkWhenEnabled, handler, scrollDistance, scrollEnabled;
+      link: function(scope, element, attrs) {
         $window = angular.element($window);
-        scrollDistance = 0;
-        if (attrs.infiniteScrollDistance != null) {
-          scope.$watch(attrs.infiniteScrollDistance, function(value) {
-            return scrollDistance = parseInt(value, 10);
-          });
-        }
-        scrollEnabled = true;
-        checkWhenEnabled = false;
-        if (attrs.infiniteScrollDisabled != null) {
-          scope.$watch(attrs.infiniteScrollDisabled, function(value) {
-            scrollEnabled = !value;
-            if (scrollEnabled && checkWhenEnabled) {
-              checkWhenEnabled = false;
-              return handler();
+        var d = document,
+            documentHeight = Math.max(d.documentElement.clientHeight, d.body.scrollHeight, d.documentElement.scrollHeight, d.body.offsetHeight, d.documentElement.offsetHeight),
+            scrollEnabled = true,
+            checkWhenEnabled = false,
+            distanceToTrigger = 200;
+        
+        var handler = function() {
+            var windowHeight = $window.innerHeight || d.documentElement.clientHeight || d.getElementsByTagName('body')[0].clientHeight,
+                scrollTop = d.documentElement.scrollTop || d.body.scrollTop,
+                top = documentHeight - windowHeight - distanceToTrigger,
+                shouldScroll = scrollTop >= top;
+        
+            if (shouldScroll && scrollEnabled) {
+                if ($rootScope.$$phase) {
+                    return scope.$eval(attrs.infiniteScroll);
+                } else {
+                    return scope.$apply(attrs.infiniteScroll);
+                }
+            } else if (shouldScroll) {
+                return checkWhenEnabled = true;
             }
-          });
-        }
-        handler = function() {
-          var elementBottom, remaining, shouldScroll, windowBottom;
-          windowBottom = $window.height() + $window.scrollTop();
-          elementBottom = elem.offset().top + elem.height();
-          remaining = elementBottom - windowBottom;
-          shouldScroll = remaining <= $window.height() * scrollDistance;
-          if (shouldScroll && scrollEnabled) {
-            if ($rootScope.$$phase) {
-              return scope.$eval(attrs.infiniteScroll);
-            } else {
-              return scope.$apply(attrs.infiniteScroll);
-            }
-          } else if (shouldScroll) {
-            return checkWhenEnabled = true;
-          }
         };
-        $window.on('scroll', handler);
+        
+        if (!attrs.infiniteScrollDisabled) {
+            scope.$watch(attrs.infiniteScrollDisabled, function(value) {
+                scrollEnabled = !value;
+        
+                if(scrollEnabled && checkWhenEnabled) {
+                    checkWhenEnabled = false;
+        
+                    return handler();
+                }
+            });
+        }
+        
+        $window.bind('scroll', handler);
+        
         scope.$on('$destroy', function() {
-          return $window.off('scroll', handler);
+            return $window.unbind('scroll', handler);
         });
+        
         return $timeout((function() {
-          if (attrs.infiniteScrollImmediateCheck) {
-            if (scope.$eval(attrs.infiniteScrollImmediateCheck)) {
-              return handler();
+            if (attrs.infiniteScrollImmediateCheck) {
+                if (scope.$eval(attrs.infiniteScrollImmediateCheck)) {
+                    return handler();
+                }
+            } else {
+                return handler();
             }
-          } else {
-            return handler();
-          }
         }), 0);
       }
     };
